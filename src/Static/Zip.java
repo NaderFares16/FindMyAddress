6a7888479b52544784bf592bfeb9b4f83a1816ea
Nader Fares <naderalifares16@hotmail.com>
2024-10-27 19:40:13 -0300

    include: Atxy2k Library

--- src/Static/Zip.java
+++ src/Static/Zip.java
@@ -17,17 +17,18 @@ import java.awt.Rectangle;
 import java.awt.SystemColor;
 import java.awt.Cursor;
 import javax.swing.ImageIcon;
+import javax.swing.DefaultComboBoxModel;
 
 public class Zip extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField textCEP;
+	private JTextField textAddress;
 	private JLabel lblNewLabel_1;
 	private JLabel lblEndereo_1;
-	private JTextField textField_3;
-	private JTextField textField_2;
+	private JTextField textDistrict;
+	private JTextField textCity;
 	private JLabel lblUf;
 	
 	public static void main(String[] args) {
@@ -60,22 +61,22 @@ public class Zip extends JFrame {
 		lblNewLabel.setBounds(32, 45, 45, 13);
 		contentPane.add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Arial", Font.PLAIN, 10));
-		textField.setBounds(137, 42, 96, 19);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		textCEP = new JTextField();
+		textCEP.setFont(new Font("Arial", Font.PLAIN, 10));
+		textCEP.setBounds(137, 42, 96, 19);
+		contentPane.add(textCEP);
+		textCEP.setColumns(10);
 		
 		JLabel lblEndereo = new JLabel("Endereço:");
 		lblEndereo.setFont(new Font("Arial", Font.BOLD, 14));
 		lblEndereo.setBounds(32, 103, 73, 13);
 		contentPane.add(lblEndereo);
 		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Arial", Font.PLAIN, 10));
-		textField_1.setColumns(10);
-		textField_1.setBounds(137, 100, 332, 19);
-		contentPane.add(textField_1);
+		textAddress = new JTextField();
+		textAddress.setFont(new Font("Arial", Font.PLAIN, 10));
+		textAddress.setColumns(10);
+		textAddress.setBounds(137, 100, 332, 19);
+		contentPane.add(textAddress);
 		
 		lblNewLabel_1 = new JLabel("Bairro:");
 		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 14));
@@ -87,48 +88,49 @@ public class Zip extends JFrame {
 		lblEndereo_1.setBounds(32, 219, 82, 13);
 		contentPane.add(lblEndereo_1);
 		
-		textField_3 = new JTextField();
-		textField_3.setFont(new Font("Arial", Font.PLAIN, 10));
-		textField_3.setColumns(10);
-		textField_3.setBounds(137, 158, 332, 19);
-		contentPane.add(textField_3);
+		textDistrict = new JTextField();
+		textDistrict.setFont(new Font("Arial", Font.PLAIN, 10));
+		textDistrict.setColumns(10);
+		textDistrict.setBounds(137, 158, 332, 19);
+		contentPane.add(textDistrict);
 		
-		textField_2 = new JTextField();
-		textField_2.setFont(new Font("Arial", Font.PLAIN, 10));
-		textField_2.setColumns(10);
-		textField_2.setBounds(142, 216, 178, 19);
-		contentPane.add(textField_2);
+		textCity = new JTextField();
+		textCity.setFont(new Font("Arial", Font.PLAIN, 10));
+		textCity.setColumns(10);
+		textCity.setBounds(142, 216, 178, 19);
+		contentPane.add(textCity);
 		
 		lblUf = new JLabel("UF:");
 		lblUf.setFont(new Font("Arial", Font.BOLD, 14));
 		lblUf.setBounds(341, 219, 25, 13);
 		contentPane.add(lblUf);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(376, 215, 73, 21);
-		contentPane.add(comboBox);
+		JComboBox comboUF = new JComboBox();
+		comboUF.setModel(new DefaultComboBoxModel(new String[] {"", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"}));
+		comboUF.setBounds(376, 215, 73, 21);
+		contentPane.add(comboUF);
 		
-		JButton btnNewButton = new JButton("Limpar");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnClear = new JButton("Limpar");
+		btnClear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(32, 277, 85, 21);
-		contentPane.add(btnNewButton);
+		btnClear.setBounds(32, 277, 85, 21);
+		contentPane.add(btnClear);
 		
 		JButton btnBuscar = new JButton("Buscar");
 		btnBuscar.setBounds(260, 41, 85, 21);
 		contentPane.add(btnBuscar);
 		
-		JButton btnNewButton_1 = new JButton("");
-		btnNewButton_1.setToolTipText("Sobre");
-		btnNewButton_1.setActionCommand("");
-		btnNewButton_1.setIcon(new ImageIcon(Zip.class.getResource("/ZIP/about.png")));
-		btnNewButton_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_1.setBorder(null);
-		btnNewButton_1.setBackground(SystemColor.control);
-		btnNewButton_1.setBounds(new Rectangle(0, 0, 32, 32));
-		btnNewButton_1.setBounds(376, 35, 32, 32);
-		contentPane.add(btnNewButton_1);
+		JButton btnAbout = new JButton("");
+		btnAbout.setToolTipText("Sobre");
+		btnAbout.setActionCommand("");
+		btnAbout.setIcon(new ImageIcon(Zip.class.getResource("/ZIP/about.png")));
+		btnAbout.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnAbout.setBorder(null);
+		btnAbout.setBackground(SystemColor.control);
+		btnAbout.setBounds(new Rectangle(0, 0, 32, 32));
+		btnAbout.setBounds(376, 35, 32, 32);
+		contentPane.add(btnAbout);
 	}
 }
